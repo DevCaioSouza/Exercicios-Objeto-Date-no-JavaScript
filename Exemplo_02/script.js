7478d5efcfa01f9ef77601a06ecdf90aff6c27d0
unknown <caiocbahia@hotmail.com>
2022-08-29 10:19:43 -0300

    retornando data atual em formato string

--- Exemplo_02/script.js
+++ Exemplo_02/script.js
@@ -1,5 +1,11 @@
-const data = new Date()
+const data = new Date();
 
-const dia = String(data.getDate())
+const dia = String(data.getDate()).padStart(2, "0");
 
-console.log(dia)
+const mes = String(data.getMonth()+1).padStart(2, "0");
+
+const ano = String(data.getFullYear()).padStart(2, "0");
+
+const dataAtual = `${dia}/${mes}/${ano}`
+
+console.log(dataAtual)
