fc7cae7759fcaca8abc2da81c844ca34f2b1a302
unknown <caiocbahia@hotmail.com>
2022-08-29 11:09:51 -0300

    Task n 01 feita

---
+++ Tasks/script.js
@@ -0,0 +1,8 @@
+//criar um objeto do tipo date com a data "01/10/2023"
+const randomDateA = new Date('2023-10-01 12:00:00');
+
+console.log(randomDateA);
+
+const randomDateB = new Date(2023,9,01);//Mês vai de 0 a 9
+
+console.log(randomDateB)
