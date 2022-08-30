f2455f7733e5721f41a2d1a489640cfbad7ac746
unknown <caiocbahia@hotmail.com>
2022-08-30 10:16:36 -0300

    concluindo task

--- Exemplo_02/script.js
+++ Exemplo_02/script.js
@@ -6,11 +6,12 @@ const mes = String(data.getMonth()+1).padStart(2, "0");
 
 const ano = String(data.getFullYear()).padStart(2, "0");
 
-const dataAtual = `${dia}/${mes}/${ano}`
+const dataAtual = `${dia}/${mes}/${ano}`;
 
-const randomDate = new Date(2023,10,01)
+const randomDate = new Date(2023,10,01);
 
 //só isso n eh suficiente pra retornar string
-const randomDateString = new Date('2023-10-01 12:00:00')
+//tem que botar 12:00:00 pq por default ele retorna 00:00:00;
+const randomDateString = new Date('2023-10-01 12:00:00');
 
-console.log(randomDateString)
+console.log(randomDateString);
